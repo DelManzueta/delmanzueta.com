542ad2fa5d63f5d6ba92f14225c018f6877a19c8
DelManzueta <dmanzueta@gmail.com>
2023-05-05 02:09:40 -0400

    simplifying resume dl for now

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,10 @@ import React, { useEffect, useState } from 'react';
 import MenuIcon from './MenuIcon';
 import headerStyles from '../assets/styles/components/header.module.css';
 
-import resume from '../assets/docs/dm.pdf';
+import resume from '../assets/docs/dm.pdf'
 
 function Header() {
 	const [isScrolled, setIsScrolled] = useState(false);
-	const [showModal, setShowModal] = useState(false);
 
 	useEffect(() => {
 		function handleScroll() {
@@ -26,67 +25,30 @@ function Header() {
 		};
 	}, []);
 
-	const handleResumeClick = () => {
-		setShowModal(true);
-	};
-
-	const handleCloseModal = () => {
-		setShowModal(false);
-	};
-
 	return (
-		<>
-			<header
-				className={`${headerStyles.header} ${
-					isScrolled ? headerStyles.scrolled : ''
-				}`}
+		<header
+			className={`${headerStyles.header} ${
+				isScrolled ? headerStyles.scrolled : ''
+			}`}
+		>
+			<Link
+				to='top'
+				spy={true}
+				smooth={true}
+				offset={-70}
+				duration={500}
+				className={headerStyles.headerBrand}
+				onClick={() => scroll.scrollToTop()}
 			>
-				<Link
-					to='top'
-					spy={true}
-					smooth={true}
-					offset={-70}
-					duration={500}
-					className={headerStyles.headerBrand}
-					onClick={() => scroll.scrollToTop()}
-				>
-					Del Manzueta
-				</Link>
-				<nav className={headerStyles.headerNav}>
-					<button
-						className={headerStyles.resumeLink}
-						onClick={handleResumeClick}
-					>
-						Resumé
-					</button>
-				</nav>
-				<div className={headerStyles.headerMenuIcon}>
-					<MenuIcon />
-				</div>
-			</header>
-			{showModal && (
-				<div className={headerStyles.modalOverlay}>
-					<div className={headerStyles.modalContent}>
-						<button
-							className={headerStyles.closeButton}
-							onClick={handleCloseModal}
-						>
-							X
-						</button>
-						<iframe
-							src={resume}
-							title='Resume'
-						/>
-						<a
-							href={resume}
-							download
-						>
-							Download
-						</a>
-					</div>
-				</div>
-			)}
-		</>
+				Del Manzueta
+			</Link>
+			<nav className={headerStyles.headerNav}>
+				<a href={resume} download>Resumé</a>
+			</nav>
+			<div className={headerStyles.headerMenuIcon}>
+				<MenuIcon />
+			</div>
+		</header>
 	);
 }
 
